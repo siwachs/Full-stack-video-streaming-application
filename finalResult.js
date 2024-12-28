8341dfdc8dd752b518d71882f2cabfa7cec98345
siwachs <ssiwach.8888@gmail.com>
2024-12-28 22:12:58 +0530

    finalResult

---
+++ finalResult.js
@@ -0,0 +1,5 @@
+// Now we have video segments and it stream with m3u8 index
+// No we can load segment Lazly insted of fetching whole video BLOB
+
+// For Adaptive HLS
+// Use mpeg command to generate other formats and m3u8 keep track with network and automatically switch formats
